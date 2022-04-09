413eb172bb3ad4ea13ffddd826660b7b564e038b
62030273 <62030273@go.buu.ac.th>
2022-04-10 02:51:54 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -67,12 +67,12 @@ Route::get('/', function () {
     return view('welcomeHOME');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
