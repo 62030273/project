c18f020cf947161cc04b3b47e072e72e5685211d
62030273 <62030273@go.buu.ac.th>
2022-04-09 17:20:54 +0700

    commmit

--- routes/web.php
+++ routes/web.php
@@ -24,27 +24,22 @@ route::resource('class_check_student','App\Http\Controllers\ClassCheckStudentCon
 route::delete('class_check_student.destroy/{ccs_cc_id}/{ccs_std_code}','App\Http\Controllers\ClassCheckStudentController@destroy')->name('classcheckstudent.destroy');
 route::resource('elle','App\Http\Controllers\StatusQuiz3Controller');
 route::resource('quiz3','App\Http\Controllers\Quiz3Controller');
-
-//  ยังไม่ทำ 
- //  ยังไม่ทำ 
-route::resource('course','App\Http\Controllers\CourseController');//  ยังไม่ทำ
-//  ยังไม่ทำ
-route::resource('enroll','App\Http\Controllers\EnrollController');//  ยังไม่ทำ
-route::delete('enroll.destroy/{enr_sect}/{enr_seq}','App\Http\Controllers\EnrollController@destroy')->name('enroll.destroy');//ยังทำส่ง2ตัวแปรไม่ได้
-route::resource('exam','App\Http\Controllers\ExamController');//  ยังไม่ทำ
+route::resource('course','App\Http\Controllers\CourseController');
+route::resource('enroll','App\Http\Controllers\EnrollController');
+route::delete('enroll.destroy/{enr_sect}/{enr_seq}','App\Http\Controllers\EnrollController@destroy')->name('enroll.destroy');
+route::resource('exam','App\Http\Controllers\ExamController');
 route::delete('exam.destroy/{ex_id}/{ex_std_code}','App\Http\Controllers\ExamController@destroy')->name('exam.destroy');
-route::resource('exam_control','App\Http\Controllers\ExamControlController');//  ยังไม่ทำ
+route::resource('exam_control','App\Http\Controllers\ExamControlController');
 route::delete('exam_control.destroy/{exc_id}','App\Http\Controllers\ExamControlController@destroy')->name('exam_control.destroy');
-route::resource('exam_question','App\Http\Controllers\ExamQuestionController');//  ยังไม่ทำ
+route::resource('exam_question','App\Http\Controllers\ExamQuestionController');
 route::delete('exam_question.destroy/{eq_ex_id}/{eq_qs_id}','App\Http\Controllers\ExamQuestionController@destroy')->name('exam_question.destroy');
-route::resource('faculty','App\Http\Controllers\FacultyController');//  ยังไม่ทำ
-route::resource('question','App\Http\Controllers\QuestionController');//  ยังไม่ทำ
+route::resource('faculty','App\Http\Controllers\FacultyController');
+route::resource('question','App\Http\Controllers\QuestionController');
 route::delete('question.destroy/{qs_id}','App\Http\Controllers\QuestionController@destroy')->name('question.destroy');
-route::resource('student','App\Http\Controllers\StudentController');//  ยังไม่ทำ
-route::resource('teacher','App\Http\Controllers\TeacherController');// 
-route::resource('teacher_teach','App\Http\Controllers\TeacherTeachController');//  
+route::resource('student','App\Http\Controllers\StudentController');
+route::resource('teacher','App\Http\Controllers\TeacherController');
+route::resource('teacher_teach','App\Http\Controllers\TeacherTeachController'); 
 route::delete('teacher_teach.destroy/{tt_crs_code}/{tt_tch_code}','App\Http\Controllers\TeacherTeachController@destroy')->name('teacher_teach.destroy');
-//  ยังไม่ทำ
 
 
 Route::get('/', function () {
